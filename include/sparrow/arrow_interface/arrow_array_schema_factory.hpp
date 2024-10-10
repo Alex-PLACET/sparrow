ec8c7ad56979bd8ff177125c2aaab62e8351967e
Alexis Placet <alexis.placet@quantstack.net>
2024-10-10 10:26:07 +0200

    compilation fix

--- include/sparrow/arrow_interface/arrow_array_schema_factory.hpp
+++ include/sparrow/arrow_interface/arrow_array_schema_factory.hpp
@@ -98,7 +98,7 @@ namespace sparrow
         return make_arrow_array(length, null_count, offset, std::move(value_buffers), 0, nullptr, nullptr);
     }
 
-    constexpr ArrowSchema make_primitive_arrow_schema(
+    ArrowSchema make_primitive_arrow_schema(
         data_type data_type,
         std::string_view name,
         std::optional<std::string_view> metadata,
